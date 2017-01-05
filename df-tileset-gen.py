bdc67fe13f30c131b0c435a27a296d8ccd07586d
Philip Jones <pjj313@student.bham.ac.uk>
2017-01-05 23:41:35 +0100

    Added shade and fill character drawing

--- df-tileset-gen.py
+++ df-tileset-gen.py
@@ -7,6 +7,70 @@ import math
 
 gettext.install("gimp20-python", gimp.locale_directory, unicode=True)
 
+def draw_shade_char(img, layer, size, density, x, y):
+    for j in xrange(4):
+        for k in xrange(j % 4, size, 4):
+            pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE,
+                    x + k, y + j, 1, 1)
+            pdb.gimp_edit_bucket_fill_full(layer, FG_BUCKET_FILL, NORMAL_MODE, 100,
+                    255, False, True, SELECT_CRITERION_COMPOSITE, 0, 0)
+
+    for j in map(lambda x: 4 * 2**x,
+            xrange(int(math.ceil(math.log(size, 2))))):
+        copy = pdb.gimp_layer_copy(layer, False)
+        pdb.gimp_image_insert_layer(img, copy, None, -1)
+        pdb.gimp_layer_translate(copy, 0, j)
+        layer = pdb.gimp_image_merge_down(img, copy, CLIP_TO_BOTTOM_LAYER)
+
+    if density == 1:
+        copy1 = pdb.gimp_layer_copy(layer, False)
+        copy2 = pdb.gimp_layer_copy(layer, False)
+        pdb.gimp_image_insert_layer(img, copy1, None, -1)
+        pdb.gimp_image_insert_layer(img, copy2, None, -1)
+        pdb.gimp_layer_translate(copy1, 2, 0)
+        pdb.gimp_layer_translate(copy2, -2, 0)
+        layer = pdb.gimp_image_merge_down(img, copy1, CLIP_TO_BOTTOM_LAYER)
+        layer = pdb.gimp_image_merge_down(img, copy2, CLIP_TO_BOTTOM_LAYER)
+    if density == 2:
+        copy1 = pdb.gimp_layer_copy(layer, False)
+        copy2 = pdb.gimp_layer_copy(layer, False)
+        copy3 = pdb.gimp_layer_copy(layer, False)
+        copy4 = pdb.gimp_layer_copy(layer, False)
+        pdb.gimp_image_insert_layer(img, copy1, None, -1)
+        pdb.gimp_image_insert_layer(img, copy2, None, -1)
+        pdb.gimp_image_insert_layer(img, copy3, None, -1)
+        pdb.gimp_image_insert_layer(img, copy4, None, -1)
+        pdb.gimp_layer_translate(copy1, 1, 0)
+        pdb.gimp_layer_translate(copy2, -1, 0)
+        pdb.gimp_layer_translate(copy3, 3, 0)
+        pdb.gimp_layer_translate(copy4, -3, 0)
+        layer = pdb.gimp_image_merge_down(img, copy1, CLIP_TO_BOTTOM_LAYER)
+        layer = pdb.gimp_image_merge_down(img, copy2, CLIP_TO_BOTTOM_LAYER)
+        layer = pdb.gimp_image_merge_down(img, copy3, CLIP_TO_BOTTOM_LAYER)
+        layer = pdb.gimp_image_merge_down(img, copy4, CLIP_TO_BOTTOM_LAYER)
+    pdb.gimp_selection_none(img)
+
+def draw_fill_char(img, layer, size, index, x, y):
+    if index == 0:
+        pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE,
+                x, y, size, size)
+    elif index == 1:
+        pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE,
+                x, y + size / 2, size, size / 2)
+    elif index == 2:
+        pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE,
+                x, y, size / 2, size)
+    elif index == 3:
+        pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE,
+                x + size / 2, y, size / 2, size)
+    elif index == 4:
+        pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE,
+                x, y, size, size / 2)
+    pdb.gimp_edit_bucket_fill_full(layer, FG_BUCKET_FILL, NORMAL_MODE, 100,
+                255, False, True, SELECT_CRITERION_COMPOSITE, 0, 0)
+    pdb.gimp_selection_none(img)
+
+
 def generate_tileset(cur_img, drawable, font, size, softening):
 
     gimp.context_push()
@@ -55,12 +119,34 @@ def generate_tileset(cur_img, drawable, font, size, softening):
 
     for i in xrange(256):
         if i in excepts:
+            pdb.gimp_progress_update(i / 255.0)
+            continue
+        x = (i % 16) * size
+        y = math.floor(i / 16.0) * size
+        if i in shade_chars:
+            layer = pdb.gimp_layer_new(img, size, size, GRAYA_IMAGE, chars[i],
+                        100, NORMAL_MODE)
+            pdb.gimp_image_insert_layer(img, layer, None, -1)
+            pdb.gimp_layer_translate(layer, x, y)
+            draw_shade_char(img, layer, size, i - shade_chars[0], x, y)
+            pdb.gimp_progress_update(i / 255.0)
+            continue
+        if i in range(179, 219):
+            pdb.gimp_progress_update(i / 255.0)
+            continue
+        if i in fill_chars:
+            layer = pdb.gimp_layer_new(img, size, size, GRAYA_IMAGE, chars[i],
+                        100, NORMAL_MODE)
+            pdb.gimp_image_insert_layer(img, layer, None, -1)
+            pdb.gimp_layer_translate(layer, x, y)
+            draw_fill_char(img, layer, size, i - fill_chars[0], x, y)
+            pdb.gimp_progress_update(i / 255.0)
             continue
 
         layer = pdb.gimp_text_layer_new(img, chars[i],
                 font, size * 3/4, UNIT_POINT)
         pdb.gimp_image_insert_layer(img, layer, None, -1)
-        pdb.gimp_layer_translate(layer, (i % 16) * size, math.floor(i / 16.0) * size)
+        pdb.gimp_layer_translate(layer, x, y)
         w = pdb.gimp_drawable_width(layer)
         pdb.gimp_layer_translate(layer, (size - w) / 2.0, 0)
         pdb.gimp_progress_update(i / 255.0)
@@ -85,7 +171,7 @@ register(
         "RGB*, GRAY*",
         [
             (PF_FONT, "font", "Font", "DejaVu Sans Mono"),
-            (PF_INT, "size", "Tile size (px)", 72),
+            (PF_INT, "size", "Tile size (px)", 96),
             (PF_SLIDER, "softening", "Background opacity", 20, (0.0, 100.0,
                 1.0))
             ],
